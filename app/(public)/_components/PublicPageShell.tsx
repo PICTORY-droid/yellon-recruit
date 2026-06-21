42907c143b014e646b45caac2ec5ee6dceafc4c6
PICTORY-droid <oceanlumes@gmail.com>
2026-06-21 12:03:16 +0900

    refactor: connect public mobile navigation shell

--- app/(public)/_components/PublicPageShell.tsx
+++ app/(public)/_components/PublicPageShell.tsx
@@ -1,3 +1,5 @@
+import { PublicBottomNavigation } from "@/app/(public)/_components/PublicBottomNavigation";
+
 type PublicPageShellProps = {
   children: React.ReactNode;
 };
@@ -5,9 +7,11 @@ type PublicPageShellProps = {
 export function PublicPageShell({ children }: PublicPageShellProps) {
   return (
     <main className="min-h-screen bg-[#eef8ff] text-[#1f3347]">
-      <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-4 pb-24 pt-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-4 pb-24 pt-4 sm:px-6 lg:px-8 lg:pb-8">
         {children}
       </div>
+
+      <PublicBottomNavigation />
     </main>
   );
 }
